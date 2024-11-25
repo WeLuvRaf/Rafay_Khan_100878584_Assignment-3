3714bb479738fec8b5fc28b4f2428691f1cfde00
WeLuvRaf <rafay.khan1@ontariotechu.net>
2024-11-24 19:58:42 -0500

    Everything laid out

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/Assignment_Tracker', function (req, res, next) {
+  res.render('Assignment_Tracker', { title: 'Assignment Tracker' });
+});
+
 module.exports = router;
